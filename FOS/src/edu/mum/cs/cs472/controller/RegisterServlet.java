b0a93ef5cfd78b631d99761faf1310cb0758d144
Davaabayar <busdavaabayar@gmail.com>
2019-11-19 14:53:38 -0600

    Register

--- FOS/src/edu/mum/cs/cs472/controller/RegisterServlet.java
+++ FOS/src/edu/mum/cs/cs472/controller/RegisterServlet.java
@@ -21,38 +21,28 @@ public class RegisterServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("RegisterServlet: doPost");
-        //Copying all the input parameters in to local variables
         String first_name = request.getParameter("first_name");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        User user = new User();
-        //Using Java Beans - An easiest way to play with group of related data
-        user.setFirstName;
-        user.setEmail(email);
-        user.setUserName(userName);
-        user.setPassword(password);
-
-        UserDAO userDAO = new UserDAO();
-
-        //The core Logic of the Registration application is present here. We are going to insert user data in to the database.
-        String userRegistered = userDAO.registerUser(userBean);
-
-        if(userRegistered.equals("SUCCESS"))   //On success, you can display a message to user on Home page
-        {
-            request.getRequestDispatcher("/Home.jsp").forward(request, response);
-        }
-        else   //On Failure, display a meaningful message to the User.
-        {
+        User newUser = new User();
+        newUser.setFirstName(first_name);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        String regResult = newUser.registerUser(newUser);
+        if(regResult.equals("SUCCESS"))  {
+            System.out.println("Success");
+            request.getRequestDispatcher("home.jsp").forward(request, response);
+        } else {
+            System.out.println("Failure");
             request.setAttribute("errMessage", userRegistered);
-            request.getRequestDispatcher("/Register.jsp").forward(request, response);
+            request.getRequestDispatcher("register").forward(request, response);
         }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.print("im here do get");
-
-        System.out.print(userService.getAllUser());
+        request.getServletContext().getRequestDispatcher("/register.jsp").forward(request, response);
     }
 
 }
