66e35111addb0a4156e6e4dfc4a5eb6dab69f001
Tuvshintur Batzorig <tuvshintubatzorig@gmail.com>
2019-11-19 12:35:21 -0600

    update

---
+++ FOS/src/edu/mum/cs/cs472/controller/RegisterServlet.java
@@ -0,0 +1,34 @@
+package edu.mum.cs.cs472.controller;
+
+import edu.mum.cs.cs472.dao.impl.UserServiceImpl;
+import edu.mum.cs.cs472.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "RegisterServlet", urlPatterns = "/register", description = "RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+
+    private UserService userService;
+
+    public RegisterServlet() {
+        userService = new UserServiceImpl();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        userService.getAllUser();
+
+        System.out.print("im here do get");
+
+        System.out.print(userService.getAllUser().size());
+    }
+
+}
---
+++ FOS/src/edu/mum/cs/cs472/service/UserService.java
@@ -0,0 +1,11 @@
+package edu.mum.cs.cs472.service;
+
+import edu.mum.cs.cs472.dao.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public List<User> getAllUser();
+
+}
