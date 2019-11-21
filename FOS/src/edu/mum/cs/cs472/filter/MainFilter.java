0f94cd83183162bec2b7da80f954d48112c798bd
Tuvshintur Batzorig <tuvshintubatzorig@gmail.com>
2019-11-20 18:49:09 -0600

    update

--- FOS/src/edu/mum/cs/cs472/filter/MainFilter.java
+++ FOS/src/edu/mum/cs/cs472/filter/MainFilter.java
@@ -16,16 +16,6 @@ public class MainFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest rq = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-        Cookie[] cookies = rq.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("JSESSIONID")) {
-                    System.out.println("JSESSIONID=" + cookie.getValue());
-                    break;
-                }
-            }
-        }
-
         String path = rq.getRequestURI();
 
         if (path.equals("/FOS/") || path.contains("/login") || path.contains("/register") || path.contains("/logout") || path.contains("/upload") || path.contains("/js/")) {
